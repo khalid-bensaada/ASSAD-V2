3e59be82ec11b153ebacfd110bc72bcb9064ed52
khalid-bensaada <kbensaada953@gmail.com>
2025-12-25 11:27:51 +0100

    added a prop of classe of animale in file of animale

--- classes/animal.php
+++ classes/animal.php
@@ -1,1 +1,14 @@
- 
+<?php
+class Animale {
+    private $id ;
+    private $name ;
+    private $alimentation;
+    private $image ;
+    private $live ;
+    private $description ;
+    private $id_habitat ;
+    private $views_animal ;
+    
+
+}
+?>
